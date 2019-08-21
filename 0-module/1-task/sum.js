d3031d4784dc5437ccc00265dccf041eea2dccd8
Mikhail Firsov <devfirsov@yandex-team.ru>
2019-08-22 00:30:53 +0300

    add solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
-  /* ваш код */
+  const args = [].slice.call(arguments)
+  if (args.some(arg => typeof arg === 'number')) {
+    return args.reduce((sum, cur) => sum + cur)
+  } else {
+    throw new TypeError('invalid data')
+  }
 }
 
 module.exports = sum;
